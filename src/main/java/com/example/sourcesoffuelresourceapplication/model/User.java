20cf4f16047bb8f96450fda3c4122a09c90eafea
serzh <sergey.over3000@gmail.com>
2022-10-21 19:52:31 +0300

    created User entity

--- src/main/java/com/example/sourcesoffuelresourceapplication/model/User.java
+++ src/main/java/com/example/sourcesoffuelresourceapplication/model/User.java
@@ -1,8 +1,32 @@
 package com.example.sourcesoffuelresourceapplication.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
     private Long id;
+
+    @Column(name= "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 }
