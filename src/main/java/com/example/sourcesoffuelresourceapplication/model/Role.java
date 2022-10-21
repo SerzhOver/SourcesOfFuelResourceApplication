3d2403ec384f9bf3ac22fe38359210d8296a9fff
serzh <sergey.over3000@gmail.com>
2022-10-21 19:53:23 +0300

    added user repository and user service

---
+++ src/main/java/com/example/sourcesoffuelresourceapplication/model/Role.java
@@ -0,0 +1,24 @@
+package com.example.sourcesoffuelresourceapplication.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "role")
+    private String role;
+
+}
---
+++ src/main/java/com/example/sourcesoffuelresourceapplication/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.sourcesoffuelresourceapplication.service;
+
+import com.example.sourcesoffuelresourceapplication.auth.ApplicationUserDetails;
+import com.example.sourcesoffuelresourceapplication.model.User;
+import com.example.sourcesoffuelresourceapplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public ApplicationUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findUserByUsername(username);
+
+        return new ApplicationUserDetails(optionalUser
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found")));
+    }
+}
